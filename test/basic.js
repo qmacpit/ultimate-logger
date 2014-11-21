e7b235ee2befd39ef274677f85805d295cb2872d
Maciek Pitucha <m.pitucha@camlintechnologies.com>
2014-11-21 13:05:33 +0100

    workin on format

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,17 @@
 var assert = require("assert"),
     expect = require('expect.js'),
+    TestModule = require("./testModule.js"),
     UltiLogger = require("../lib/ultimate-logger");
+
+
+(function(){
+    console.__log = console.log;
+    console.log = function() {
+        console.__log.apply(console, arguments);
+        return arguments[0];
+    }    
+})();
+
 
 describe('DBusConnector suite', function(){
 
@@ -12,6 +23,14 @@ describe('DBusConnector suite', function(){
         expect(console.notice).to.be.ok();
         expect(console.info).to.be.ok();
         expect(console.debug).to.be.ok();
+
+    });    
+
+    it('caller data check', function(){
+
+        var log = TestModule.info("info")
+        console.log(log)
+        TestModule.error("error")
 
     });    
 
