d0faefadcc3b95c5e7d4ed8356915bde889b6fa1
Maciek Pitucha <m.pitucha@camlintechnologies.com>
2014-12-02 11:06:17 +0100

    0.0.5

--- test/advanced.js
+++ test/advanced.js
@@ -2,12 +2,15 @@ var assert = require("assert"),
     expect = require('expect.js'),
     TestModule = require("./testModule.js"),
     FooBar = require("./fooBar.js"),    
+    Foo = require("./foo.js"),    
     _console = {
-        log: _loggingMethod
+        log: _loggingMethod,
+        logCounter: 0
     },
     UltiLogger = require("../lib/ultimate-logger")(_console);
 
-function _loggingMethod(msg) {    
+function _loggingMethod(msg) { 
+    _console.logCounter++;     
     return msg;    
 }
 
@@ -17,10 +20,12 @@ describe('Ultimate logger - advanced suite', function(){
         UltiLogger.enable();
         TestModule.setConsole(_console);
         FooBar.setConsole(_console);
+        Foo.setConsole(_console);
     });
 
     beforeEach(function(){
         UltiLogger.setFilter(null);
+        UltiLogger.setStackLevel(1);
     });
 
     it('depth basic check', function(){
@@ -40,7 +45,30 @@ describe('Ultimate logger - advanced suite', function(){
         log = FooBar.foo(msg);
         expect(log).to.eql("fooBar:foo():6 " + msg);
 
-    });   
+    });  
+
+    it('no printout/logging method call from excluded module', function(){
+
+        var msg = "testMsg", log;
+        
+        UltiLogger.setFormat("{{moduleName}}: ");
+        UltiLogger.setFilter({
+            moduleName: "!foo"
+        });
+        _console.logCounter = 0;
+        log = Foo.foo(msg);
+        expect(_console.logCounter).to.eql(0);
+
+        log = FooBar.foo(msg);        
+        expect(log).to.eql("bar: " + msg);
+
+        _console.logCounter = 0;
+        log = FooBar.foo(msg);        
+        expect(_console.logCounter).to.eql(1);
+        log = Foo.foo(msg);
+        expect(_console.logCounter).to.eql(1);
+
+    });    
 });   
 
 
